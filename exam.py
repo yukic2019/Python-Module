7db98cceed2d9ff06dd83bbe71838d0c67f14a65
yukic2019 <58543484+yukic2019@users.noreply.github.com>
2019-12-05 13:46:11 +0800

    Create exam.py

---
+++ exam.py
@@ -0,0 +1,9 @@
+exam_data = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'],
+             'score': [12.5, 9, 16.5, 3, 9, 20, 20, 7, 8, 19],
+             'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+             'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
+
+
+import pandas as pd
+exam=pd.DataFrame(exam_data)
+print(exam)
